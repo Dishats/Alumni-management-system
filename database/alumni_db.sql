fa08830ff79388a4867f85fb0009d409cc655c42
Dishats <155805719+Dishats@users.noreply.github.com>
2025-05-14 22:15:25 +0530

    Update alumni_db.sql

--- database/alumni_db.sql
+++ database/alumni_db.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 16, 2020 at 05:17 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+Database: `alumni_db`
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+--------------------------------------------------------
 
---
--- Database: `alumni_db`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `alumnus_bio`
+Table structure for table `alumnus_bio`
 --
 
 CREATE TABLE `alumnus_bio` (
